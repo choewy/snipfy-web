f36e2523ce422360aba10264edaeda22dee7901f
choewy <choewy@aininetworks.com>
2025-04-10 18:02:59 +0900

    chore: 에러 모달 설정

--- src/common/modal/modal.component.tsx
+++ src/common/modal/modal.component.tsx
@@ -1,2 +1,22 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+
+import { useErrorModalStore } from '../../store/error-modal.store';
+
 // TODO
-export class ModalComponent {}
+export class ModalComponent {
+  public static ErrorModal() {
+    const { open, message, onClose } = useErrorModalStore();
+
+    return (
+      <Dialog open={open} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+        <DialogTitle id="alert-dialog-title">ERROR</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{message}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button size="small" sx={{ color: '#1e293b' }} onClick={onClose}></Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
